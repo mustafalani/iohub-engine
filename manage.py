7f69f151dd8b58ff5c58ba539ede3573eb9729ea
Mustafa <mustafalani@gmail.com>
2020-12-28 11:37:06 +0100

    init

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iohubengine.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iohub-engine.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
